911b0cee5e0d08a922ae4c2da19636ae58b61b32
Kumar137 <38612435+Kumar137@users.noreply.github.com>
2018-07-11 08:49:39 +0530

    Added Controller for user sign in and user sign up

---
+++ Course5_Project/Exercise-master/src/main/java/org/upgrad/repositories/User_Profile.java
@@ -0,0 +1,20 @@
+package org.upgrad.repositories;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.upgrad.models.UserProfile;
+import org.upgrad.models.Users;
+
+import javax.transaction.Transactional;
+import java.util.Date;
+
+@Repository
+public interface User_Profile extends CrudRepository<UserProfile, String> {
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="INSERT INTO USER_PROFILE(user_id,firstName,lastName,aboutMe,dob,contactNumber,country) VALUES(?1,?2,?3,?4,?5,?6,?7)")
+    void userProfileDetails(int user_id, String firstName, String lastName, String aboutMe, Date dob, String contactNumber, String country);
+}
