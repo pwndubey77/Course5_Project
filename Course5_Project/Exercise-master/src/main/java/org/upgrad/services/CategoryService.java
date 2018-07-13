8fb709c3b368deb85bb0159a60b642440da73bcc
pawan dubey <pwndubey77@gmail.com>
2018-07-13 22:40:28 +0530

    updating category Services

--- Course5_Project/Exercise-master/src/main/java/org/upgrad/services/CategoryService.java
+++ Course5_Project/Exercise-master/src/main/java/org/upgrad/services/CategoryService.java
@@ -1,4 +1,13 @@
 package org.upgrad.services;
 
-public class CategoryService {
+import org.upgrad.models.Category;
+import org.upgrad.models.UserProfile;
+
+import java.util.Date;
+import java.util.Set;
+
+public interface CategoryService {
+    void createCategory(int id,String title,String description);
+    Category getCategory(int categoryId);
+    //Set<Integer> getQuestionByCategory(int categoryId);
 }
