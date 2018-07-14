a33b9ffcfb27a8535a01cf4dfb42c570d7d5857e
Spoorthi S Madhavan <36541492+spoorthimadhavan@users.noreply.github.com>
2018-07-14 15:39:51 +0530

    Adding CommentRepository

---
+++ Course5_Project/Exercise-master/src/main/java/org/upgrad/repositories/CommentRepository.java
@@ -0,0 +1,36 @@
+package org.upgrad.repositories;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.upgrad.models.Comment;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends CrudRepository<Comment,String>{
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="insert into comment (content,date ,user_id,answer_id) values (?1,CURRENT_TIMESTAMP ,?2,?3)")
+    void giveComment(int commentId,String content, int userId );
+
+    @Query(nativeQuery = true,value="select user_id from answer where id = ?1")
+    int getUserByAnswerId(int commentid);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="update content set content=?2,modifiedon=now() where id=?1")
+    void editComment(int commentId , String content);
+
+    @Query(nativeQuery = true,value ="select * from content where answer_id = ?1")
+    List<Comment> getAllCommentsByAnswer (int answerId);
+
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="delete from comment where id=?1 ")
+    void deleteComment(int commentId);
+}
