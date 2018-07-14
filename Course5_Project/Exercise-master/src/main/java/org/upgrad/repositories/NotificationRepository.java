ed75104a1d648b9b65f595c4ff4d322edeaf62ec
pwndubey77 <pwndubey77@gmail.com>
2018-07-14 12:35:37 +0530

    add notifcatio repo update

--- Course5_Project/Exercise-master/src/main/java/org/upgrad/repositories/NotificationRepository.java
+++ Course5_Project/Exercise-master/src/main/java/org/upgrad/repositories/NotificationRepository.java
@@ -1,4 +1,18 @@
 package org.upgrad.repositories;
 
-public interface NotificationRepository {
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.upgrad.models.Notification;
+
+import javax.transaction.Transactional;
+
+@Repository
+public interface NotificationRepository extends CrudRepository<Notification,Integer>{
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value="insert into notification (user_id,message ,date,read) values (?1,?2,CURRENT_TIMESTAMP,FALSE )")
+    void addNotificationForUser(int user, String notificationMessage);
 }
