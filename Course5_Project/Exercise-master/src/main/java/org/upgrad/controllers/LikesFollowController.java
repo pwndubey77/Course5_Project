010aae61fb5b2ba389c16f7bb68827b249e62b1c
pwndubey77 <pwndubey77@gmail.com>
2018-07-15 09:46:18 +0530

    Updatinh Http status

--- Course5_Project/Exercise-master/src/main/java/org/upgrad/controllers/LikesFollowController.java
+++ Course5_Project/Exercise-master/src/main/java/org/upgrad/controllers/LikesFollowController.java
@@ -65,7 +65,7 @@ public class LikesFollowController {
                 return new ResponseEntity<>("You have unliked answer with answerId " +answerId+ " successfully", HttpStatus.OK);
             }
             else{
-                return new ResponseEntity<>("You have not liked this answer", HttpStatus.UNAUTHORIZED);
+                return new ResponseEntity<>("You have not liked this answer", HttpStatus.NOT_FOUND);
             }
 
         }
